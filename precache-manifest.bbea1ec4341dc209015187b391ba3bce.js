9a61629ff0d02aee7eef686bd4cac3c670ff56c5
ag502 <ag502@naver.com>
2020-10-02 18:15:25 +0900

    Updates

---
+++ precache-manifest.bbea1ec4341dc209015187b391ba3bce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53df049079aa2d58830ba91af508776e",
+    "url": "/react_basic_habit_tracker/index.html"
+  },
+  {
+    "revision": "7cfbd96801c5a2bc0cd8",
+    "url": "/react_basic_habit_tracker/static/css/main.7640f2ec.chunk.css"
+  },
+  {
+    "revision": "ed45ae1a6102ce3bc176",
+    "url": "/react_basic_habit_tracker/static/js/2.b32eb186.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react_basic_habit_tracker/static/js/2.b32eb186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfbd96801c5a2bc0cd8",
+    "url": "/react_basic_habit_tracker/static/js/main.b2c1caca.chunk.js"
+  },
+  {
+    "revision": "cce1584d664dd6fcc7a5",
+    "url": "/react_basic_habit_tracker/static/js/runtime-main.929c0c59.js"
+  }
+]);
